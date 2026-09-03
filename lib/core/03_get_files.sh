8cb485c2b9c9f6188628e70b46d2b00b97451af8
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-03 21:56:30 +0100

    feat(core): add get files function

---
+++ lib/core/03_get_files.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# shellcheck source=/dev/null
+# shellcheck disable=2155
+# file handler script
+
+get_files() {
+    local directory="$1"
+    local -a files=()
+    mapfile -t files < <(find "$directory" -mindepth 1 -maxdepth 1  -type f 2> /dev/null)
+    [[ ${#files[@]} -eq 0 ]] && {
+        printf "No file found in %s" "$directory"
+        info "No file found in $directory"
+        exit 0
+    }
+
+    for file in "${files[@]}"; do
+        printf "%s\n" "$file"
+    done
+}
+
+store_files() {
+    local -a files
+    directory="$(find_all_matches)"
+    mapfile -t files < <( get_files "$directory" )
+    [[ ${#files[@]} -eq 0 ]] && {
+        printf "No file found in %s" "$directory"
+        info "No file found in $directory"
+        exit 0
+    }
+    for file in "${files[@]}"; do
+        printf "%s\n" "$file"
+        echo extension: $(get_extension)
+    done
+}
+
+get_extension() {
+    local file_path="$1"
+    extension=${file_path##*.}
+    printf "%s" "$extension"
+}
+
+
+get_file_count() {
+    echo pass
+}
+
+get_all_mtimes() {
+    echo pass
+
+}
