8b082c546a094bad53be710cd9f007367b2ab549
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-05 12:45:55 +0100

    fix(move): add create dir function

--- lib/functions/0_move.sh
+++ lib/functions/0_move.sh
@@ -3,6 +3,21 @@
 # shellcheck disable=2154
 # move function script
 
+
+create_dir() {
+    local dir="$1"
+    local sub_dir="$2"
+    [[ ! -d "$dir" && ! -w "$dir" ]] && {
+        mkdir -p "$dir/$sub_dir" || {
+        echo "Failed to create directory $dir/$sub_dir" >&2
+        echo "move operation failed" >&2
+        error "move operation failed"
+        error "Failed to create directory $dir/$sub_dir"
+        exit 1
+    }
+        dir="$dir/$sub_dir"
+    }
+}
 move_file() {
     local file="$1"
     local destination="$2"
@@ -25,14 +40,7 @@ move_by_extension() {
     printf "Enter the destination directory: "
     read -r destination
     [[ ! -d "$destination" && ! -w "$destination" ]] && {
-        mkdir -p "$SCRIPT_DIR/$destination" || {
-        echo "Failed to create directory $destination/$extension" >&2
-        echo "move operation failed" >&2
-        error "move operation failed"
-        error "Failed to create directory $destination/$extension"
-        exit 1
-    }
-        destination="$SCRIPT_DIR/$destination"
+        create_dir "$SCRIPT_DIR" "$destination"
     }
     
     while IFS= read -r file; do 
