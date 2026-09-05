584b1c9f6817aaddb79c925cff444d930ef59f1f
Kengah Ireneaus <4jrkit1@gmail.com>
2026-09-05 14:42:48 +0100

    feat(execute): add precaution function

--- lib/functions/3_execute.sh
+++ lib/functions/3_execute.sh
@@ -3,34 +3,69 @@
 # shellcheck disable=2154
 # execute function script
 
+
+run_precaution() {
+    read -r answer
+    [[ "$answer" != "[Y][y]" ]] && {
+        echo "Operation cancelled by user" >&2
+        exit 1
+    }
+}
 execute_sub_option() {
     local sub_option="$1"
     case "$sub_option" in
         "delete_empty_files")
+            printf "Are you sure you want to delete empty files? [Y/y] to confirm: "
+            run_precaution
+            clear
             delete_empty_files
         ;;
         "delete_large_files")
+            printf "Are you sure you want to delete large files? [Y/y] to confirm: "
+            run_precaution
+            clear
             delete_large_files
         ;;
         "delete_old_files")
+            printf "Are you sure you want to delete old files? [Y/y] to confirm: "
+            run_precaution
+            clear
             delete_old_files
         ;;
         "delete_by_extension")
+            printf "Are you sure you want to delete files by extension? [Y/y] to confirm: "
+            run_precaution
+            clear
             delete_by_extension
         ;;
         "move_by_extension")
+            printf "Are you sure you want to move files by extension? [Y/y] to confirm: "
+            run_precaution
+            clear
             move_by_extension
         ;;
         "move_by_date")
+            printf "Are you sure you want to move files by date? [Y/y] to confirm: "
+            run_precaution
+            clear
             move_by_date
         ;;
         "auto_arrange_by_extension")
+            printf "Are you sure you want to auto arrange files by extension? [Y/y] to confirm: "
+            run_precaution
+            clear
             auto_arrange_by_extension
         ;;
         "auto_arrange_by_size")
+            printf "Are you sure you want to auto arrange files by size? [Y/y] to confirm: "
+            run_precaution
+            clear
             auto_arrange_by_size
         ;;
         "auto_arrange_by_mtime")
+            printf "Are you sure you want to auto arrange files by modification time? [Y/y] to confirm: "
+            run_precaution
+            clear
             auto_arrange_by_mtime
         ;;
         *)
